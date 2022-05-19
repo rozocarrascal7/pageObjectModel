72ac941d49e87c8c43c5ffa330be8b8d7e2f6532
rozocarrascal7 <38230971+rozocarrascal7@users.noreply.github.com>
2022-05-19 09:14:03 -0500

    update other things inside sauceLogin pages

--- src/test/java/pages/sauceLogin.java
+++ src/test/java/pages/sauceLogin.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 public class sauceLogin {
 
     /**
-     * Locators and methodss
+     * Locators and methods of the sauceLogin pages
      */
     By usernameField = By.id("user-name");
     By passwordField = By.name("password");
